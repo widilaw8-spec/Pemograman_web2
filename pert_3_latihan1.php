dd0272ebd64a0454a6e507e928c689f72ee75b08
widilaw8-spec <widilaw8@gmail.com>
2026-09-24 21:19:08 +0700

    pert3_latihan1

---
+++ pert_3_latihan1.php
@@ -0,0 +1,26 @@
+<?php
+$ball = 'purple'; // Coba ubah ke "yellow", "blue", "green", "purple", atau warna lain
+
+$redbox = $yellowbox = $bluebox = $greenbox = $purplebox = $colorlessbox = "-";
+
+if ($ball == 'red') {
+  $redbox = $ball;
+} elseif ($ball == 'yellow') {
+  $yellowbox = $ball;
+} elseif ($ball == 'blue') {
+  $bluebox = $ball;
+} elseif ($ball == 'green') {
+  $greenbox = $ball;
+} elseif ($ball == 'purple') {
+  $purplebox = $ball;
+} else {
+  $colorlessbox = $ball;
+}
+
+echo "red box : $redbox <br>\n";
+echo "yellow box : $yellowbox <br>\n";
+echo "blue box : $bluebox <br>\n";
+echo "green box : $greenbox <br>\n";
+echo "purple box : $purplebox <br>\n";
+echo "colorless box : $colorlessbox <br>\n";
+?>
